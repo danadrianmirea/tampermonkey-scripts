9dec29bf98810187ca3a64bfd56a124b2673ebee
Dan Adrian Mirea <damirea84@gmail.com>
2026-04-02 14:51:32 +0300

    add first version of block aliexpress preview script

---
+++ BlockAliexpressPreview.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Block Aliexpress Preview
+// @namespace    http://tampermonkey.net/
+// @version      2026-04-02
+// @description  Block annoying aliexpress preview popup on over
+// @match        https://www.aliexpress.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=aliexpress.com
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/danadrianmirea/filter-tradingview-ideas/main/filterTradingview.js
+// @downloadURL  https://raw.githubusercontent.com/danadrianmirea/filter-tradingview-ideas/main/filterTradingview.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
